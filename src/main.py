2ef381278ba1608a64c3be7957cf8576053bed64
Vladislav Gorodiskiy <wdatboy@icloud.com>
2026-05-03 11:04:13 +0300

    Create src/ directory for scripts

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+import os
+
+
+def print_author():
+    load_dotenv(dotenv_path='.env')
+    author = os.getenv('AUTHOR')
+    print(f"Автор проекта: {author}")
+
+if __name__ == "__main__":
+    print_author()
